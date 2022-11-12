e5bfa7a9f9129fb381fdc270b90af29033614cab
gehiner-windows <gehinerferley@gmail.com>
2022-11-11 19:22:58 -0500

    Implementacion con el Backend

--- src/Componentes/Tablas/Productos.js
+++ src/Componentes/Tablas/Productos.js
@@ -3,40 +3,61 @@ import "../../Styles/index-admin/index-admin-contend.css"
 import "../../Styles/index-admin/index-admin-Desktop.css"
 import "../../Styles/index-admin/index-admin-table-productos.css"
 import "../../Styles/index-admin/index-admin-table.css"
-import CategoriasServicios from "../../Servicios/CategoriasServicios"
+import ProductosServicios from "../../Servicios/ProductosServicios"
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom"
 import HeaderAdmin from "../Header/HeaderAdmin"
 import Estados from "../../Enums/Estados"
 const Productos=()=>{
-    const [ estado, setEstado ] = useState(Estados.CARGANDO);
-    const [ listaCategorias, setListacategorias ] = useState([]);
-    const [ busqueda, setBusqueda ]=useState("");
+    const [estado, setEstado] = useState(Estados.CARGANDO);
+    const [listaProductos, setListaProductos] = useState([]);
+    const [busqueda, setBusqueda]=useState("");
 
-    const buscarCategorias=
+    const cargarDatos = async () => {
+        try {
+            const respuesta = await ProductosServicios.listarProductos();
+            if (respuesta.data.length > 0) {
+                setEstado(Estados.OK);
+                setListaProductos(respuesta.data);
+                console.log(respuesta.data)
+            }
+            else {
+                setEstado(Estados.VACIO);
+            }
+        } catch (error) {
+            setEstado(Estados.ERROR);
+            console.log(error);
+        }
+    }
+
+    const criterio=(event)=>{
+        setBusqueda(event.target.value);
+        console.log(busqueda);
+    }
 
     useEffect(() => {
-        const cargarDatos = async () => {
-            try {
-                const respuesta = await CategoriasServicios.listarCategorias();
-                if (respuesta.data.lenght > 0 ) {
-                    setListacategorias(respuesta);
-                    setEstado(Estados.OK);
-                    console.log(listaCategorias);
-                }
-                else {
-                    setEstado(Estados.VACIO);
-                }
-            } catch (error) {
-                setEstado(Estados.ERROR);
-                console.log(error);
-            }
-        }
         cargarDatos();
     }, [])
+
+    const buscarProducto=async(event)=>{
+        event.preventDefault();
+        try {
+            const respuesta = await ProductosServicios.buscarproducts(busqueda);
+            console.log(respuesta);
+            if (respuesta.data.length > 0) {
+                setListaProductos(respuesta.data);
+                setEstado(Estados.OK)
+            } else {
+                setEstado(Estados.VACIO)
+            }
+        } catch (error) {
+            setEstado(Estados.ERROR);
+            console.log(error);
+        }
+    }
     return(
-        <body>
+        <div>
             <HeaderAdmin/>
             <main>
                 <section className="sidebar" id="opciones-menu">
@@ -56,7 +77,7 @@ const Productos=()=>{
                         <img src="https://i.ibb.co/7y7t2LX/bxs-user-circle-White.webp" alt="Icono de Usuario"/>
                         <span>Usuarios</span>
                     </a>
-                    <a className="sidebar-option disabled" href="">
+                    <a className="sidebar-option disabled" href="/">
                         <img src="https://i.ibb.co/K0VRBHk/bxs_log-out-circle-White.webp" alt=""/>
                         <span>Cerrar Sesi&oacute;n</span>
                     </a>
@@ -78,16 +99,16 @@ const Productos=()=>{
                         <img src="https://i.ibb.co/7y7t2LX/bxs-user-circle-White.webp" alt="Icono de Usuario"/>
                         <span>Usuarios</span>
                     </Link>
-                    <a className="sidebar-option disabled" href="">
-                        <img src="https://i.ibb.co/K0VRBHk/bxs_log-out-circle-White.webp" alt=""/>
+                    <a className="sidebar-option disabled" href="/">
+                        <img src="https://i.ibb.co/K0VRBHk/bxs_log-out-circle-White.webp" alt="/"/>
                         <span>Cerrar Sesi&oacute;n</span>
                     </a>
                 </section>
                 <section className="main-container">
                     <h2 className="main-title">Productos</h2>
                     <form action="" className="main-search">
-                        <input type="text" placeholder="B&uacute;squeda"/>
-                        <button type="submit"><img src="https://i.ibb.co/n8b6R5s/akar-icons-search.webp" alt=""/></button>
+                        <input type="text" placeholder="B&uacute;squeda" onChange={criterio} value={busqueda}/>
+                        <button  onClick={buscarProducto}><img src="https://i.ibb.co/n8b6R5s/akar-icons-search.webp" alt=""/></button>
                     </form>
                     <div className="main-table-container table-big">
                         <div className="main-table__header table ">
@@ -123,81 +144,41 @@ const Productos=()=>{
                             </div>
                         </div>
                         {
-                                estado === Estados.ERROR ? (
-                                    <div>Ocurrió un error...</div>
-                                ) 
+                            estado === Estados.ERROR ? (<div>Ocurrió un error...</div>):
+                                Estados.VACIO ? (<div>No hay Datos...</div>)
                                 :
-                                estado === Estados.CARGANDO ? (
-                                    <div>CARGANDO</div>
-                                ) 
-                                :
-                                estado === Estados.VACIO ? (
-                                    <div>No hay datos</div>
-                                )
-                                :                    
-                                listaCategorias.map((categoria)=>(
-                                    <a className="main-table__contend table" href="" key={categoria.id}>
+                                listaProductos.map((producto)=>(
+                                    <div className="main-table__contend table" href="" key={producto._id}>
                                         <div className="main-table__contend-info">
-                                            {categoria.nombre}
+                                            {producto.nombre}
                                         </div>
                                         <div className="main-table__contend-info">
-                                            {categoria.marca}
+                                            {producto.marca}
                                         </div>
                                         <div className="main-table__contend-info">
-                                            dadad
+                                            {producto.tipo}
                                         </div>
                                         <div className="main-table__contend-info">
-                                            adad
+                                            {producto.disponibilidad ? "En Bodega":"Sin Disponibilidad"}
                                         </div>
                                         <div className="main-table__contend-info">
-                                            5000
+                                            {producto.precio}
                                         </div>
                                         <div className="main-table__contend-info">
-                                            25
+                                            {producto.cantidad}
                                         </div>
                                         <div className="main-table__contend-info main-table__header-title-big">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quasi corporis esse quia culpa tempore necessitatibus voluptate quidem cupiditate ipsum maxime 
+                                            {producto.descripcion} 
                                         </div>
                                         <div className="main-table__contend-info">
-                                            Crocante
+                                            {producto.keywords}
                                         </div>
                                         <div className="main-table__contend-info">
-                                            wwww.png.com
-                                        </div>
-                                    </a>
-                            ))                
-                        } 
-                                    <div className="main-table__contend table" href="" >
-                                        <div className="main-table__contend-info">
-                                            1
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            2
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            dadad
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            adad
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            5000
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            25
-                                        </div>
-                                        <div className="main-table__contend-info main-table__header-title-big">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora quasi corporis esse quia culpa tempore necessitatibus voluptate quidem cupiditate ipsum maxime 
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            Crocante
-                                        </div>
-                                        <div className="main-table__contend-info">
-                                            wwww.png.com
+                                            {producto.url_image}
                                         </div>
                                         <div className="main-table__contend-info main-table__opciones">
                                             <div className="opciones-container">
-                                                <a href="/productos/edit">
+                                                <a href={"/productos/edit/" + producto._id} >
                                                     <img src="https://cdn-icons-png.flaticon.com/512/3838/3838756.png" alt="Icono editar"/>
                                                 </a>
                                                 <a href="">
@@ -205,11 +186,13 @@ const Productos=()=>{
                                                 </a>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div>     
+                                ))
+                        }
                     </div>
                 </section>
             </main>
-        </body>    
+        </div>    
     )
 }
 
